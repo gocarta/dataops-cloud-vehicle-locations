c3d666dab05073dd1577e3994f6bd23bbe52aa87
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2026-03-28 22:07:46 -0400

    alpha

---
+++ main.py
@@ -0,0 +1,114 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "boto3",
+#     "datablob",
+#     "simple-env",
+#     "tzdata",
+# ]
+# ///
+import datablob
+from datetime import datetime, timedelta
+import simple_env as se
+import time
+
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+from zoneinfo import ZoneInfo
+
+AWS_DYNAMODB_TABLE_NAME = se.get("AWS_DYNAMODB_TABLE_NAME")
+DATAOPS_TIMEZONE = se.get("DATAOPS_TIMEZONE")
+AWS_BUCKET_NAME = se.get("AWS_BUCKET_NAME")
+AWS_BUCKET_PATH = se.get("AWS_BUCKET_PATH")
+AWS_DYNAMODB_REGION = se.get("AWS_DYNAMODB_REGION")
+
+timezone = ZoneInfo(DATAOPS_TIMEZONE)
+timezone_utc = ZoneInfo("UTC")
+
+VEHICLE_IDS = list(sorted(se.get("VEHICLE_IDS").strip().split(",")))
+
+vehicle_ids = [(vid, vid.strip().zfill(4)) for vid in VEHICLE_IDS]
+print("vehicle_ids:", vehicle_ids)
+
+dynamodb = boto3.resource("dynamodb", region_name=AWS_DYNAMODB_REGION)
+table = dynamodb.Table(AWS_DYNAMODB_TABLE_NAME)
+
+while True:
+
+    try:
+        time.sleep(5)
+
+        start_time = time.perf_counter()
+
+        rows = []
+
+        for vehicle_id, vid in vehicle_ids:
+            # Querying the partition key for the newest single item
+            response = table.query(
+                KeyConditionExpression=Key("vehicleId").eq(vid),
+                FilterExpression=Attr("age").eq("Fresh"),
+                ScanIndexForward=False,  # Sorts descending (newest first)
+                Limit=1,  # Only grab the top result
+            )
+
+            items = response.get("Items", [])
+
+            if not items:
+                # print(f"[dataops-cloud-vehicle-locations] no data found for vehicle: {vid}")
+                continue
+
+            item = items[0]
+
+            timestamp = float(item["timestamp"])
+
+            received_utc = datetime.fromtimestamp(timestamp, tz=timezone_utc)
+
+            # 3. Create initial Reported datetime using Received's UTC date
+            reported_time_obj = datetime.strptime(item["time"], "%H:%M:%S").time()
+            reported_utc = datetime.combine(
+                received_utc.date(), reported_time_obj, tzinfo=timezone_utc
+            )
+
+            # If reported is > 60 seconds after received, it belongs to the previous day
+            if reported_utc > (received_utc + timedelta(minutes=1)):
+                reported_utc -= timedelta(days=1)
+
+            now_utc = datetime.now(ZoneInfo("UTC"))
+            threshold = timedelta(minutes=5)
+
+            age = (now_utc - reported_utc).total_seconds()
+
+            row = {
+                "vehicle_id": vehicle_id,
+                "latitude": float(item["latitude"]),
+                "longitude": float(item["longitude"]),
+                "reported": reported_utc.astimezone(timezone).isoformat(),
+                "age": int(age),
+            }
+
+            rows.append(row)
+
+
+        client = datablob.DataBlobClient(
+            bucket_name=AWS_BUCKET_NAME, bucket_path=AWS_BUCKET_PATH
+        )
+
+        client.update_dataset(
+            name="cloud_vehicle_locations",
+            version="1",
+            data=rows,
+            description="Real-Time Location of CARTA Fixed-Route Buses and Shuttles.  Each vehicle sends its location via TAIP message every 5 seconds to AWS.",
+            latitude_key="latitude",
+            longitude_key="longitude",
+            xlsx=True,
+        )
+
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        print("execution_time:", execution_time)
+
+        print(f"[dataops-cloud-vehicle-locations] updated {len(rows)} rows")
+
+    except Exception as e:
+        print(e)
